ef105f17a00cdeb6da7ce64f122c8bec75c9847d
Julien Blin <julien.blin@gmail.com>
2018-05-19 14:28:10 -0400

    Added log to lambda-authorizer.

--- src/lambda-authorizer.ts
+++ src/lambda-authorizer.ts
@@ -1,5 +1,6 @@
 // tslint:disable-next-line:no-implicit-dependencies
 import * as lambda from "aws-lambda";
+import { defaultConfidentialityReplacer } from "./utils";
 
 export interface LambdaAuthorizerBearerFunctionArgs {
   bearerToken?: string;
@@ -10,12 +11,41 @@ export interface LambdaAuthorizerBearerFunctionArgs {
 export type LambdaAuthorizerBearerFunction =
   (args: LambdaAuthorizerBearerFunctionArgs) => Promise<lambda.CustomAuthorizerResult>;
 
+export interface LambdaAuthorizerBearerError {
+  context: lambda.Context;
+  error: any;
+  event: lambda.CustomAuthorizerEvent;
+}
+
+export interface LambdaAuthorizerBearerOptions {
+  /**
+   * The custom error logger to use.
+   * If not provided, will use console.error.
+   */
+  errorLogger?(error: LambdaAuthorizerBearerError): void | Promise<void>;
+}
+
+const defaultErrorLogger = async (error: LambdaAuthorizerBearerError) => {
+
+  const payload = {
+    authorizationToken: error.event.authorizationToken,
+    error: error.error.toString(),
+    errorStackTrace: error.error.stack,
+    headers: error.event.headers,
+    requestContext: error.event.requestContext,
+  };
+
+  const JSON_STRINGIFY_SPACE = 2;
+
+  console.error(JSON.stringify(payload, defaultConfidentialityReplacer, JSON_STRINGIFY_SPACE));
+};
+
 /**
  * Creates a wrapper for a Lambda authorizer function for a bearer token.
  * @param func - The function to wrap.
  */
 export const lambdaAuthorizerBearer =
-  (func: LambdaAuthorizerBearerFunction): lambda.CustomAuthorizerHandler =>
+  (func: LambdaAuthorizerBearerFunction, options: LambdaAuthorizerBearerOptions = {}): lambda.CustomAuthorizerHandler =>
     async (event: lambda.CustomAuthorizerEvent, context: lambda.Context, callback: lambda.CustomAuthorizerCallback)
     : Promise<lambda.CustomAuthorizerResult> => {
 
@@ -23,9 +53,26 @@ export const lambdaAuthorizerBearer =
         ? event.authorizationToken.replace(/\s*bearer\s*/ig, "")
         : undefined;
 
-      return func({
+      try {
+        return await func({
           bearerToken,
           context,
           event,
         });
+      } catch (error) {
+        if (!options.errorLogger) {
+          options.errorLogger = defaultErrorLogger;
+        }
+
+        try {
+          const loggerPromise = options.errorLogger({ event, context, error });
+          if (loggerPromise) {
+            await loggerPromise;
+          }
+        } catch (loggerError) {
+          console.error(loggerError);
+        }
+
+        throw error;
+      }
     };
--- tests/lambda-authorizer-tests.ts
+++ tests/lambda-authorizer-tests.ts
@@ -2,7 +2,7 @@
 import { CustomAuthorizerResult } from "aws-lambda";
 import { expect } from "chai";
 import { describe, it } from "mocha";
-import { lambdaAuthorizerBearer } from "../src/lambda-authorizer";
+import { lambdaAuthorizerBearer, LambdaAuthorizerBearerError } from "../src/lambda-authorizer";
 import { createLambdaContext, randomStr } from "./lambda-helper-tests";
 
 // tslint:disable:newline-per-chained-call
@@ -36,6 +36,36 @@ describe("lambdaAuthorizerBearer", () => {
       (e, r) => {}) as CustomAuthorizerResult;
 
     expect(lambdaResult.principalId).equal(inputBearerToken);
+  });
+
+  it("should handle errors", async () => {
+    const inputBearerToken = "a987sfasd785sadf876";
+    const errorMessage = "This is an error.";
+    let loggedLambdaError: LambdaAuthorizerBearerError | undefined;
+
+    const lambda = lambdaAuthorizerBearer(
+      async () => { throw new Error(errorMessage); },
+      {
+        errorLogger: (lambdaError) => { loggedLambdaError = lambdaError; },
+      });
+
+    try {
+      await lambda(
+        {
+          authorizationToken: `bearer ${inputBearerToken}`,
+          methodArn: randomStr(),
+          type: randomStr(),
+        },
+        createLambdaContext(),
+        (e, r) => {});
+
+      expect(false);
+    } catch (error) {
+      expect(loggedLambdaError).not.be.undefined;
+      expect(loggedLambdaError!.event).to.not.be.undefined;
+      expect(loggedLambdaError!.context).to.not.be.undefined;
+      expect(loggedLambdaError!.error.message).to.equal(errorMessage);
+    }
   });
 
 });
