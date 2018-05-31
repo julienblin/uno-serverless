01ac120abad97b12714624e96ca37234dc792a5f
Julien Blin <julien.blin@nventive.com>
2018-05-31 19:23:11 -0400

    Added JSONFileConfigService.

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,6 @@
+import { readFile } from "fs";
 import { configurationError } from "./errors";
+import { checkHealth, HealthCheckResult, ICheckHealth } from "./health-checks";
 
 // tslint:disable:max-classes-per-file
 
@@ -53,4 +55,68 @@ export class ProcessEnvConfigService extends StaticConfigService {
     super(env, name);
   }
 
+}
+
+/** Options for JSONFileConfigService */
+export interface JSONFileConfigServiceOptions {
+  /** If true, the file is reloaded on each get. */
+  debug?: boolean;
+
+  /** The file path */
+  path: string;
+}
+
+/**
+ * ConfigService implementation that loads a JSON file
+ * with key/value pairs.
+ */
+export class JSONFileConfigService implements ConfigService, ICheckHealth {
+
+  /** Cached promise for file content. */
+  private fileContent: Promise<Record<string, string>> | undefined;
+
+  public constructor(
+    private readonly options: JSONFileConfigServiceOptions,
+    private readonly name = "JSONFileConfigService") {}
+
+  /** Performs a health check. */
+  public async checkHealth(): Promise<HealthCheckResult> {
+    return checkHealth(
+      "JSONFileConfigService",
+      this.options.path,
+      async () => this.loadFile());
+  }
+
+  public async get(key: string): Promise<string>;
+  public async get(key: string, required = true): Promise<string | undefined> {
+    if (!this.fileContent || this.options.debug) {
+      this.fileContent = this.loadFile();
+    }
+
+    const resolvedParameters = await this.fileContent;
+
+    if (resolvedParameters[key]) {
+      return resolvedParameters[key];
+    }
+
+    if (required) {
+      throw configurationError({ key, provider: this.name }, `Missing configuration value for ${key}`);
+    }
+
+    return undefined;
+
+  }
+
+  /** Load the file from disk and parse as JSON. */
+  private async loadFile(): Promise<Record<string, string>> {
+    return new Promise<Record<string, string>>((resolve, reject) => {
+      readFile(this.options.path, (err, data) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(JSON.parse(data.toString()));
+        }
+      });
+    });
+  }
 }
--- tests/config-tests.ts
+++ tests/config-tests.ts
@@ -1,6 +1,6 @@
 import { expect } from "chai";
 import { describe, it } from "mocha";
-import { ConfigService, ProcessEnvConfigService, StaticConfigService } from "../src/config";
+import { ConfigService, JSONFileConfigService, ProcessEnvConfigService, StaticConfigService } from "../src/config";
 
 // tslint:disable:newline-per-chained-call
 // tslint:disable:no-unused-expression
@@ -86,5 +86,49 @@ describe("ProcessEnvConfigService", () => {
       expect(error.data.provider).to.equal("ProcessEnvConfigService");
     }
   });
+});
 
+describe("JSONFileConfigService", () => {
+
+  it("should return mandatory values", async () => {
+    const config = new JSONFileConfigService({
+      path: "./tests/config-tests.json",
+    }) as ConfigService;
+
+    const result = await config.get("foo");
+    expect(result).to.equal(process.env.foo);
+  });
+
+  it("should return optional values", async () => {
+    const config = new JSONFileConfigService({
+      path: "./tests/config-tests.json",
+    }) as ConfigService;
+
+    const result = await config.get("missing", false);
+    expect(result).to.be.undefined;
+  });
+
+  it("should throw on missing required values", async () => {
+    const config = new JSONFileConfigService({
+      path: "./tests/config-tests.json",
+    }) as ConfigService;
+
+    try {
+      await config.get("missing");
+      expect(false);
+    } catch (error) {
+      expect(error.code).to.equal("configurationError");
+      expect(error.data.key).to.equal("missing");
+      expect(error.data.provider).to.equal("JSONFileConfigService");
+    }
+
+    try {
+      await config.get("missing", true);
+      expect(false);
+    } catch (error) {
+      expect(error.code).to.equal("configurationError");
+      expect(error.data.key).to.equal("missing");
+      expect(error.data.provider).to.equal("JSONFileConfigService");
+    }
+  });
 });
