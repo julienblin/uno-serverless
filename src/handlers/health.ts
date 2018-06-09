ae93f7326cde211ba51f1eff8ed316808468233b
Julien Blin <julien.blin@gmail.com>
2018-06-09 12:15:51 -0400

    Health handler now understand APIGatewayProxyEvents.

--- src/handlers/health.ts
+++ src/handlers/health.ts
@@ -1,4 +1,5 @@
 import * as awsLambda from "aws-lambda";
+import * as HttpStatusCodes from "http-status-codes";
 import { LambdaArg, LambdaExecution } from "../core/builder";
 import { checkHealth, HealthCheckRun, HealthCheckStatus } from "../services/health-check";
 
@@ -7,24 +8,54 @@ export type HealthFunc<TEvent, TServices> =
     context: awsLambda.Context,
     event: TEvent,
     services: TServices,
-   }) => Promise<HealthCheckRun[]>;
+  }) => Promise<HealthCheckRun[]>;
+
+/**
+ * Determine if result is an APIGatewayProxyEvent.
+ */
+export const isAPIGatewayProxyEvent = (value: any): value is awsLambda.APIGatewayProxyEvent =>
+  (typeof value === "object" && typeof value !== "string" && "httpMethod" in value);
 
 /**
  * This handlers runs the health checks provided.
  */
 export const health = <TEvent = any, TServices = any>(name: string, func: HealthFunc<TEvent, TServices>)
   : LambdaExecution<TEvent, TServices> => {
-    return async (arg: LambdaArg<TEvent, TServices>) => {
-      try {
-        const runs = await func(arg);
-        return await checkHealth(name, undefined, runs);
-      } catch (error) {
-        return ({
-          error,
-          message: "Error while running the health checks.",
-          name,
-          status: HealthCheckStatus.Error,
-        });
+  return async (arg: LambdaArg<TEvent, TServices>) => {
+    let result;
+    try {
+      const runs = await func(arg);
+      result = await checkHealth(name, undefined, runs);
+    } catch (error) {
+      result = {
+        error,
+        message: "Error while running the health checks.",
+        name,
+        status: HealthCheckStatus.Error,
+      };
+    }
+
+    if (isAPIGatewayProxyEvent(arg.event)) {
+      switch (result.status) {
+        case HealthCheckStatus.Ok:
+        case HealthCheckStatus.Inconclusive:
+          return {
+            body: result,
+            statusCode: HttpStatusCodes.OK,
+          };
+        case HealthCheckStatus.Warning:
+          return {
+            body: result,
+            statusCode: HttpStatusCodes.BAD_REQUEST,
+          };
+        case HealthCheckStatus.Error:
+          return {
+            body: result,
+            statusCode: HttpStatusCodes.INTERNAL_SERVER_ERROR,
+          };
       }
-    };
+    }
+
+    return result;
+  };
 };
--- test/unit/handlers/health-test.ts
+++ test/unit/handlers/health-test.ts
@@ -1,10 +1,11 @@
 import * as awsLambda from "aws-lambda";
 import { expect } from "chai";
+import * as HttpStatusCodes from "http-status-codes";
 import { lambda } from "../../../src/core/builder";
 import { randomStr } from "../../../src/core/utils";
 import { health } from "../../../src/handlers/health";
 import { HealthCheckResult, HealthCheckStatus } from "../../../src/services/health-check";
-import { createLambdaContext } from "../lambda-helper-test";
+import { createAPIGatewayProxyEvent, createLambdaContext } from "../lambda-helper-test";
 
 describe("health handler", () => {
 
@@ -23,4 +24,49 @@ describe("health handler", () => {
     expect(lambdaResult.status).to.equal(HealthCheckStatus.Inconclusive);
   });
 
+  it("should run health checks with API Events - OK", async () => {
+
+    const name = randomStr();
+    const handler = lambda()
+      .handler(health(name, async ({ }) => []));
+
+    const lambdaResult = await handler(
+      createAPIGatewayProxyEvent(),
+      createLambdaContext(),
+      (e, r) => { }) as awsLambda.APIGatewayProxyResult;
+
+    expect(lambdaResult.statusCode).to.equal(HttpStatusCodes.OK);
+  });
+
+  it("should run health checks with API Events - Warning", async () => {
+
+    const name = randomStr();
+    const handler = lambda()
+      .handler(health(name, async ({ }) => [
+        async () => ({ name: "Warning", status: HealthCheckStatus.Warning }),
+      ]));
+
+    const lambdaResult = await handler(
+      createAPIGatewayProxyEvent(),
+      createLambdaContext(),
+      (e, r) => { }) as awsLambda.APIGatewayProxyResult;
+
+    expect(lambdaResult.statusCode).to.equal(HttpStatusCodes.BAD_REQUEST);
+  });
+
+  it("should run health checks with API Events - Error", async () => {
+
+    const name = randomStr();
+    const handler = lambda()
+      .handler(health(name, async ({ }) => [
+        async () => ({ name: "Error", status: HealthCheckStatus.Error }),
+      ]));
+
+    const lambdaResult = await handler(
+      createAPIGatewayProxyEvent(),
+      createLambdaContext(),
+      (e, r) => { }) as awsLambda.APIGatewayProxyResult;
+
+    expect(lambdaResult.statusCode).to.equal(HttpStatusCodes.INTERNAL_SERVER_ERROR);
+  });
 });
