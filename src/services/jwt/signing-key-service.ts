a77325f8a8f03d8f7c078f4e4703c0b2ec9e5cff
Julien Blin <julien.blin@nventive.com>
2018-06-12 09:41:12 -0400

    Added RSSigningKeyService.

--- src/services/jwt/signing-key-service.ts
+++ src/services/jwt/signing-key-service.ts
@@ -1,11 +1,18 @@
-import { jwk2pem } from "pem-jwk";
+import { execSync } from "child_process";
+import { createHash } from "crypto";
+import { readFileSync, unlinkSync } from "fs";
+import { createPrivateKey, getPublicKey } from "pem";
+import { jwk2pem, pem2jwk } from "pem-jwk";
+import { randomStr } from "../../core";
 import { HttpClient } from "../http-client";
 
 /**
  * A component that can manages signing keys for tokens.
  */
 export interface SigningKeyService {
+  getSecretOrPrivateKey(): Promise<PrivateKeyInfo>;
   getSecretOrPublicKey(keyId?: string): Promise<string>;
+  getJWK(): Promise<JWK>;
 }
 
 /**
@@ -16,14 +23,22 @@ export interface JWKSigningKeyServiceOptions {
   jwkUrl: string | Promise<string>;
 }
 
+/**
+ * SigningKeyService that can retreive public keys
+ * from a JWK endpoint, to validate signatures.
+ */
 export class JWKSigningKeyService implements SigningKeyService {
 
   private readonly cachedKeys: Record<string, string> = {};
-  private jwkPromise: Promise<JWKResponse> | undefined;
+  private jwkPromise: Promise<JWK> | undefined;
 
   public constructor(
     private readonly options: JWKSigningKeyServiceOptions,
     private readonly httpClient: HttpClient) { }
+
+  public async getSecretOrPrivateKey(): Promise<PrivateKeyInfo> {
+    throw new Error("JWKSigningKeyService does not support getting private key.");
+  }
 
   public async getSecretOrPublicKey(keyId?: string | undefined): Promise<string> {
     if (!keyId) {
@@ -56,14 +71,105 @@ export class JWKSigningKeyService implements SigningKeyService {
     throw new Error(`JWKSigningKeyService: Unable to find key with id ${keyId}.`);
   }
 
-  private async getJWK(): Promise<JWKResponse> {
-    const response = await this.httpClient.get<JWKResponse>(await this.options.jwkUrl);
+  public async getJWK(): Promise<JWK> {
+    const response = await this.httpClient.get<JWK>(await this.options.jwkUrl);
 
     return response.data;
   }
 }
 
-interface JWKResponse {
+/**
+ * Options for SelfContainedSigningKeyService.
+ */
+export interface RSSigningKeyServiceOptions {
+  /**
+   * Base64-encoded PEM private key.
+   */
+  privateKey: string;
+}
+
+/**
+ * SigningKeyService implementation that manages RS keys.
+ */
+export class RSSigningKeyService implements SigningKeyService {
+
+  /**
+   * Generates a new private key.
+   * Needs access to file system and uses ssh-keygen (openssl) utility.
+   */
+  public static async generatePrivateKey(): Promise<string> {
+    const keyId = randomStr();
+    execSync(`ssh-keygen -t rsa -b 2048 -f ${keyId}.key -N ""`);
+    try {
+      return new Buffer(readFileSync(`${keyId}.key`, "utf8")).toString("base64");
+    } finally {
+      unlinkSync(`${keyId}.key`);
+      unlinkSync(`${keyId}.key.pub`);
+    }
+  }
+
+  private privateKey: PrivateKeyInfo | undefined;
+  private jwk: JWK | undefined;
+  private publicKey: string | undefined;
+
+  public constructor(private readonly options: RSSigningKeyServiceOptions) {}
+
+  public async getSecretOrPrivateKey(): Promise<PrivateKeyInfo> {
+    if (!this.privateKey) {
+      const privateKeyPem = new Buffer(this.options.privateKey, "base64").toString("utf8");
+      this.privateKey = {
+        key: privateKeyPem,
+        kid: createHash("md5").update(privateKeyPem).digest("hex"),
+      };
+    }
+
+    return this.privateKey;
+  }
+
+  public async getSecretOrPublicKey(keyId?: string): Promise<string> {
+    if (!this.publicKey) {
+      const privateKey = await this.getSecretOrPrivateKey();
+      this.publicKey = await new Promise<string>((resolve, reject) => {
+        getPublicKey(privateKey.key, (err, res) => {
+          if (err) {
+            reject(err);
+          } else {
+            resolve(res.publicKey);
+          }
+        });
+      });
+    }
+    return this.publicKey;
+  }
+
+  public async getJWK(): Promise<JWK> {
+    if (!this.jwk) {
+      const privateKey = await this.getSecretOrPrivateKey();
+      const publicKey = await this.getSecretOrPublicKey();
+      this.jwk = {
+        keys: [
+          {
+            ... pem2jwk(publicKey),
+            kid: privateKey.kid,
+            use: "sig",
+          },
+        ],
+      };
+    }
+
+    return this.jwk;
+  }
+}
+
+export interface PrivateKeyInfo {
+  /** The key id. */
+  kid: string;
+
+  /** The private key in PEM format. */
+  key: string;
+}
+
+export interface JWK {
   keys: Array<{
     e: string;
     kid: string;
--- test/unit/services/jwt/signing-key-service-test.ts
+++ test/unit/services/jwt/signing-key-service-test.ts
@@ -3,7 +3,9 @@ import { readFileSync } from "fs";
 import * as nock from "nock";
 import { randomStr } from "../../../../src/core";
 import { httpClientFactory } from "../../../../src/services/http-client";
-import { JWKSigningKeyService, SigningKeyService } from "../../../../src/services/jwt/signing-key-service";
+import {
+  JWKSigningKeyService, RSSigningKeyService,
+  SigningKeyService } from "../../../../src/services/jwt/signing-key-service";
 
 describe("JWKSigningKeyService", () => {
 
@@ -44,6 +46,47 @@ describe("JWKSigningKeyService", () => {
     } catch (error) {
       expect(error.message).to.contain("find key");
     }
+  });
+
+});
+
+describe("RSSigningKeyService", () => {
+
+  let privateKey: string;
+
+  before(async () => {
+    privateKey = await RSSigningKeyService.generatePrivateKey();
+  });
+
+  it("should generate private keys", async () => {
+    const key = await RSSigningKeyService.generatePrivateKey();
+    expect(key).to.not.be.undefined;
+  });
+
+  it("should return private key", async () => {
+    const service = new RSSigningKeyService({ privateKey });
+
+    const result = await service.getSecretOrPrivateKey();
+    expect(result.kid).to.not.be.undefined;
+    expect(result.key).to.not.be.undefined;
+  });
+
+  it("should return public key", async () => {
+    const service = new RSSigningKeyService({ privateKey });
+
+    const result = await service.getSecretOrPublicKey();
+    expect(result).to.not.be.undefined;
+  });
+
+  it("should return JWK", async () => {
+    const service = new RSSigningKeyService({ privateKey });
+
+    const result = await service.getJWK();
+    expect(result.keys.length).to.be.greaterThan(0);
+    const key = result.keys[0]!;
+    expect(key.kid).to.not.be.undefined;
+    expect(key.e).to.not.be.undefined;
+    expect(key.use).to.not.be.undefined;
   });
 
 });
