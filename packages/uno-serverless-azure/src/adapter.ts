71b6b7a3d7701b3e25642355e17850ec5e8276a9
Julien Blin <julien.blin@gmail.com>
2018-07-06 17:52:07 -0400

    Removed async func in adapter in favor of callback.

--- packages/uno-serverless-azure/src/adapter.ts
+++ packages/uno-serverless-azure/src/adapter.ts
@@ -9,8 +9,7 @@ const throwPrincipal = () => { throw new Error("Unable to retrieve principal. D
 export const azureFunctionAdapter = (): ProviderAdapter => {
   return () => {
     return new GenericFunctionBuilder((outerCircle) => {
-      return async (context: AzureFunctionsContext, event: any) => {
-        try {
+      return (context: AzureFunctionsContext, event: any) => {
           const unoContext: UnoContext = {
             invocationId: context.invocationId,
             log: context.log,
@@ -25,7 +24,7 @@ export const azureFunctionAdapter = (): ProviderAdapter => {
             const httpUnoEvent: HttpUnoEvent = {
               body: () => azHttpEvent.body,
               headers: azHttpEvent.headers || {},
-              httpMethod: azHttpEvent.method.toLowerCase(),
+              httpMethod: azHttpEvent.method && azHttpEvent.method.toLowerCase(),
               original: azHttpEvent,
               parameters: {
                 ...azHttpEvent.query,
@@ -47,62 +46,62 @@ export const azureFunctionAdapter = (): ProviderAdapter => {
             };
           }
 
-          const result = await outerCircle({ event: adapterEvent, context: unoContext, services: {} });
+          outerCircle({ event: adapterEvent, context: unoContext, services: {} })
+            .then((result) => {
+              switch (adapterEvent!.unoEventType) {
+                case "http":
+                  let output: AzureFunctionsHttpResponse | undefined;
 
-          switch (adapterEvent!.unoEventType) {
-            case "http":
-              let output: AzureFunctionsHttpResponse | undefined;
+                  if (!result) {
+                    output = {
+                      status: HttpStatusCode.NO_CONTENT,
+                    };
+                  }
 
-              if (!result) {
-                output = {
-                  status: HttpStatusCode.NO_CONTENT,
-                };
+                  if (result && !result.body) {
+                    output = {
+                      headers: result.headers,
+                      status: result.statusCode,
+                    };
+                  }
+
+                  if (result && typeof result.body === "string") {
+                    output = {
+                      body: result.body,
+                      headers: result.headers,
+                      isRaw: true,
+                      status: result.statusCode,
+                    };
+                  }
+
+                  if (result && Buffer.isBuffer(result.body)) {
+                    output = {
+                      body: result.body.toString("base64"),
+                      headers: result.headers,
+                      isRaw: true,
+                      status: result.statusCode,
+                    };
+                  }
+
+                  if (!output && result) {
+                    // Last resort - we rely on Azure Functions native serialization.
+                    output = {
+                      body: result.body,
+                      headers: result.headers,
+                      status: result.statusCode,
+                    };
+                  }
+
+                  context.done(undefined, output);
+                  break;
+                default:
+                  context.done(undefined, result);
+                  break;
               }
-
-              if (result && !result.body) {
-                output = {
-                  headers: result.headers,
-                  status: result.statusCode,
-                };
-              }
-
-              if (result && typeof result.body === "string") {
-                output = {
-                  body: result.body,
-                  headers: result.headers,
-                  isRaw: true,
-                  status: result.statusCode,
-                };
-              }
-
-              if (result && Buffer.isBuffer(result.body)) {
-                output = {
-                  body: result.body.toString("base64"),
-                  headers: result.headers,
-                  isRaw: true,
-                  status: result.statusCode,
-                };
-              }
-
-              if (!output && result) {
-                // Last resort - we rely on Azure Functions native serialization.
-                output = {
-                  body: result.body,
-                  headers: result.headers,
-                  status: result.statusCode,
-                };
-              }
-
-              context.done(undefined, output);
-              break;
-            default:
-              context.done(undefined, result);
-              break;
-          }
-
-        } catch (error) {
-          context.done(error);
-        }
+            })
+            .catch((error) => {
+              context.done(error);
+            });
       };
     });
   };
