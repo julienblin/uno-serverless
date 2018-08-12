069445229161380699fd77491be15a95b5836883
Julien Blin <julien.blin@gmail.com>
2018-08-12 15:57:15 -0400

    Better unit tests.

--- packages/uno-serverless-cli/test/unit/services/schema-generation-test.ts
+++ packages/uno-serverless-cli/test/unit/services/schema-generation-test.ts
@@ -1,18 +1,38 @@
+import { expect } from "chai";
+import * as yaml from "js-yaml";
 import * as path from "path";
 import { SchemaGeneration } from "../../../src/services/schema-generation";
 
-describe("SchemaGeneration", () => {
+describe("SchemaGeneration", function() {
+  this.timeout(10000);
 
   const basePath = path.resolve(__dirname);
   const files = `${basePath}/../samples/entities/*.ts`;
 
   it("should generate JSON schemas", () => {
+
     const generation = new SchemaGeneration({
       files,
       format: "json",
     });
 
     const result = generation.run();
+    const parsedResult = JSON.parse(result);
+    expect(parsedResult.definitions.User).to.not.be.undefined;
+    expect(parsedResult.definitions.User.properties.email.type).to.equal("string");
+  });
+
+  it("should generate YAML schemas", () => {
+
+    const generation = new SchemaGeneration({
+      files,
+      format: "yaml",
+    });
+
+    const result = generation.run();
+    const parsedResult = yaml.load(result) as any;
+    expect(parsedResult.definitions.User).to.not.be.undefined;
+    expect(parsedResult.definitions.User.properties.email.type).to.equal("string");
   });
 
 });
