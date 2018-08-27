2d08e8e6fa75f23b7afeafc68118551d8caef6f3
Julien Blin <julien.blin@gmail.com>
2018-08-27 18:10:31 -0400

    HttpClient: Added debug mode
    Fix #62

--- packages/uno-serverless/src/services/http-client.ts
+++ packages/uno-serverless/src/services/http-client.ts
@@ -1,5 +1,6 @@
 import Axios, * as axios from "axios";
-import { isStatusCodeProvider, lazyAsync } from "../core";
+import * as chalk from "chalk";
+import { isStatusCodeProvider, lazyAsync, safeJSONStringify } from "../core";
 
 export type PossiblePromise<T> = T | Promise<T>;
 
@@ -7,6 +8,7 @@ export interface HttpClientConfig {
   adapter?: axios.AxiosAdapter;
   auth?: PossiblePromise<axios.AxiosBasicCredentials>;
   cancelToken?: axios.CancelToken;
+  debug?: PossiblePromise<boolean>;
   baseURL?: PossiblePromise<string>;
   headers?: PossiblePromise<Record<string, string>>;
   httpAgent?: any;
@@ -32,9 +34,9 @@ export interface HttpClient {
   head(url: string, config?: axios.AxiosRequestConfig | undefined): Promise<axios.AxiosResponse<any>>;
   patch<T = any>(url: string, data?: any, config?: axios.AxiosRequestConfig | undefined);
   post<T = any>(url: string, data?: any, config?: axios.AxiosRequestConfig | undefined)
-  : Promise<axios.AxiosResponse<T>>;
+    : Promise<axios.AxiosResponse<T>>;
   put<T = any>(url: string, data?: any, config?: axios.AxiosRequestConfig | undefined)
-  : Promise<axios.AxiosResponse<T>>;
+    : Promise<axios.AxiosResponse<T>>;
   request<T = any>(config: axios.AxiosRequestConfig): Promise<axios.AxiosResponse<T>>;
 }
 
@@ -83,75 +85,75 @@ class AxiosHttpClient implements HttpClient {
 
   private readonly lazyClient = lazyAsync(async () => this.clientBuilder());
 
-  public constructor(private readonly clientBuilder: () => Promise<axios.AxiosInstance>) {}
+  public constructor(private readonly clientBuilder: () => Promise<axios.AxiosInstance>) { }
 
   public async delete(url: string, config?: axios.AxiosRequestConfig | undefined)
     : Promise<axios.AxiosResponse<any>> {
-      try {
-        return await (await this.lazyClient()).delete(url, config);
-      } catch (error) {
-        throw httpClientError(error);
-      }
+    try {
+      return await (await this.lazyClient()).delete(url, config);
+    } catch (error) {
+      throw httpClientError(error);
+    }
   }
 
   public async get<T = any>(url: string, config?: axios.AxiosRequestConfig | undefined)
     : Promise<axios.AxiosResponse<T>> {
-      try {
-        return await (await this.lazyClient()).get<T>(url, config);
-      } catch (error) {
-        throw httpClientError(error);
-      }
+    try {
+      return await (await this.lazyClient()).get<T>(url, config);
+    } catch (error) {
+      throw httpClientError(error);
+    }
   }
 
   public async head(url: string, config?: axios.AxiosRequestConfig | undefined)
     : Promise<axios.AxiosResponse<any>> {
-      try {
-        return await (await this.lazyClient()).head(url, config);
-      } catch (error) {
-        throw httpClientError(error);
-      }
+    try {
+      return await (await this.lazyClient()).head(url, config);
+    } catch (error) {
+      throw httpClientError(error);
+    }
   }
 
   public async patch<T = any>(url: string, data?: any, config?: axios.AxiosRequestConfig | undefined)
     : Promise<axios.AxiosResponse<T>> {
-      try {
-        return await (await this.lazyClient()).patch<T>(url, data, config);
-      } catch (error) {
-        throw httpClientError(error);
-      }
+    try {
+      return await (await this.lazyClient()).patch<T>(url, data, config);
+    } catch (error) {
+      throw httpClientError(error);
+    }
   }
 
   public async post<T = any>(url: string, data?: any, config?: axios.AxiosRequestConfig | undefined)
     : Promise<axios.AxiosResponse<T>> {
-      try {
-        return await (await this.lazyClient()).post<T>(url, data, config);
-      } catch (error) {
-        throw httpClientError(error);
-      }
+    try {
+      return await (await this.lazyClient()).post<T>(url, data, config);
+    } catch (error) {
+      throw httpClientError(error);
+    }
   }
 
   public async put<T = any>(url: string, data?: any, config?: axios.AxiosRequestConfig | undefined)
     : Promise<axios.AxiosResponse<T>> {
-      try {
-        return await (await this.lazyClient()).put<T>(url, data, config);
-      } catch (error) {
-        throw httpClientError(error);
-      }
+    try {
+      return await (await this.lazyClient()).put<T>(url, data, config);
+    } catch (error) {
+      throw httpClientError(error);
+    }
   }
 
   public async request<T = any>(config: axios.AxiosRequestConfig)
     : Promise<axios.AxiosResponse<T>> {
-      try {
-        return await (await this.lazyClient()).request<T>(config);
-      } catch (error) {
-        throw httpClientError(error);
-      }
+    try {
+      return await (await this.lazyClient()).request<T>(config);
+    } catch (error) {
+      throw httpClientError(error);
+    }
   }
 }
 
 export const httpClientFactory = (config: HttpClientConfig = {}): HttpClient =>
-  new AxiosHttpClient(async () =>
-    Axios.create({
+  new AxiosHttpClient(async () => {
+    const axiosInstance = Axios.create({
       adapter: config.adapter ? config.adapter : Axios.defaults.adapter,
       auth: config.auth ? await config.auth : Axios.defaults.auth,
       baseURL: config.baseURL ? await config.baseURL : Axios.defaults.baseURL,
@@ -172,4 +174,47 @@ export const httpClientFactory = (config: HttpClientConfig = {}): HttpClient =>
       transformResponse: config.transformResponse ? config.transformResponse : Axios.defaults.transformResponse,
       validateStatus: config.validateStatus ? config.validateStatus : Axios.defaults.validateStatus,
       withCredentials: config.withCredentials ? await config.withCredentials : Axios.defaults.withCredentials,
-    }));
+    });
+
+    if (await config.debug) {
+      const debug = (content, color) => {
+        console.log(chalk[color](
+          typeof content === "string" ? content : safeJSONStringify(content, undefined, 2)));
+      };
+
+      const curateRequest = (c) => {
+        if (typeof c === "string") {
+          return c;
+        }
+
+        return {
+          baseURL: c.baseURL,
+          data: c.data,
+          headers: c.headers || {},
+          method: c.method,
+          url: c.url,
+        };
+      };
+
+      const curateResponse = (response) => {
+        if (typeof response === "string") {
+          return response;
+        }
+
+        return {
+          data: response.data,
+          headers: response.headers || {},
+          status: response.status,
+        };
+      };
+
+      axiosInstance.interceptors.request.use(
+        (c) => { debug(curateRequest(c), "cyan"); return c; },
+        (error) => { debug(error, "red"); return Promise.reject(error); });
+      axiosInstance.interceptors.response.use(
+        (response) => { debug(curateResponse(response), "green"); return response; },
+        (error) => { debug(error, "red"); return Promise.reject(error); });
+    }
+
+    return axiosInstance;
+  });
