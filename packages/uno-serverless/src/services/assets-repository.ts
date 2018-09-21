02a5acc83fc6ed16c35cff8a32dd0b32743fbe68
Julien Blin <julien.blin@nventive.com>
2018-09-21 16:07:37 -0400

    Rough implementation of FileAssetsRepository.

--- packages/uno-serverless/src/services/assets-repository.ts
+++ packages/uno-serverless/src/services/assets-repository.ts
@@ -1,9 +1,14 @@
 import * as computeEtag from "etag";
+import * as fs from "fs-extra";
+import { tmpdir } from "os";
+import { dirname, join } from "path";
 import * as streamToPromise from "stream-to-promise";
+import { randomStr } from "../core/utils";
+import { checkHealth, CheckHealth } from "./health-check";
 
 export interface AssetMetadata {
-  etag: string;
-  mediaType: string;
+  etag?: string;
+  mediaType?: string;
   path: string;
 }
 
@@ -21,7 +26,10 @@ export interface SetAssetRequest {
  * Allows manipulation of assets data (e.g. binaries, images, etc.)
  */
 export interface AssetsRepository {
+  delete(path: string): Promise<void>;
+  exists(path: string): Promise<AssetMetadata | undefined>;
   get(path: string): Promise<Asset | undefined>;
+  list(prefix: string): Promise<string[]>;
   set(request: SetAssetRequest): Promise<AssetMetadata>;
 }
 
@@ -29,7 +37,7 @@ export interface AssetsRepository {
  * AssetsRepository in-memory implementation.
  * Useful for unit-testing.
  */
-export class InMemoryAssetsRepository implements AssetsRepository {
+export class InMemoryAssetsRepository implements AssetsRepository, CheckHealth {
 
   /** The storage. */
   private readonly storage = new Map<string, Asset>();
@@ -38,8 +46,39 @@ export class InMemoryAssetsRepository implements AssetsRepository {
     this.storage.clear();
   }
 
+  public async checkHealth() {
+    return checkHealth(
+      "InMemoryAssetsRepository",
+      "in-memory",
+      async () => ({}));
+  }
+
+  public async delete(path: string): Promise<void> {
+    this.storage.delete(path);
+  }
+
+  public async exists(path: string): Promise<AssetMetadata | undefined> {
+    const item = this.storage.get(path);
+    if (!item) {
+      return undefined;
+    }
+
+    const { data, ...metadata } = item;
+    return metadata;
+  }
+
   public async get(path: string): Promise<Asset | undefined> {
     return this.storage.get(path);
+  }
+
+  public async list(prefix: string): Promise<string[]> {
+    const result: string[] = [];
+    for (const key of this.storage.keys()) {
+      if (key.startsWith(prefix)) {
+        result.push(key);
+      }
+    }
+    return result;
   }
 
   public async set(request: SetAssetRequest): Promise<AssetMetadata> {
@@ -62,5 +101,158 @@ export class InMemoryAssetsRepository implements AssetsRepository {
 
     return metadata;
   }
+}
+
+/**
+ * Options for FileKeyValueRepository.
+ */
+export interface FileKeyValueRepositoryOptions {
+  /** Base path for storing files */
+  path?: string;
+}
+
+/**
+ * AssetsRepository file-based implementation.
+ * Useful for unit-testing.
+ */
+export class FileAssetsRepository implements AssetsRepository, CheckHealth {
+
+  private readonly options: Required<FileKeyValueRepositoryOptions>;
+
+  public constructor({
+    path = join(tmpdir(), encodeURIComponent(randomStr(8))),
+  }: FileKeyValueRepositoryOptions = {}) {
+    this.options = {
+      path,
+    };
+  }
+
+  public async checkHealth() {
+    return checkHealth(
+      "FileAssetsRepository",
+      this.options.path,
+      async () => {
+        const testPath = randomStr();
+        await this.set({ path: testPath, source: new Buffer(randomStr()), mediaType: "application/octet-stream" });
+        await this.delete(testPath);
+      });
+  }
+
+  public async clear(): Promise<void> {
+    return new Promise<void>((resolve, reject) => {
+      fs.exists(this.options.path, (exists) => {
+        if (!exists) {
+          resolve();
+        } else {
+          fs.remove(this.options.path, (err) => {
+            if (err) {
+              reject(err);
+            } else {
+              resolve();
+            }
+          });
+        }
+      });
+    });
+  }
+
+  public async delete(path: string): Promise<void> {
+    return new Promise<void>((resolve, reject) => {
+      fs.exists(this.filePath(path), (fileExists) => {
+        if (fileExists) {
+          fs.unlink(this.filePath(path), (unlinkErr) => {
+            if (unlinkErr) {
+              reject(unlinkErr);
+            } else {
+              resolve();
+            }
+          });
+        } else {
+          resolve();
+        }
+      });
+    });
+  }
+
+  public async exists(path: string): Promise<AssetMetadata | undefined> {
+    return new Promise<AssetMetadata | undefined>((resolve) => {
+      fs.exists(this.filePath(path), (fileExists) => {
+        resolve(fileExists ? { path } : undefined);
+      });
+    });
+  }
+
+  public async get(path: string): Promise<Asset | undefined> {
+    return new Promise<Asset | undefined>((resolve, reject) => {
+      fs.exists(this.filePath(path), (fileExists) => {
+        if (!fileExists) {
+          resolve(undefined);
+        } else {
+          fs.readFile(this.filePath(path), (fileReadError, data) => {
+            if (fileReadError) {
+              reject(fileReadError);
+            } else {
+              resolve({
+                data,
+                path,
+              });
+            }
+          });
+        }
+      });
+    });
+  }
+
+  public async list(prefix: string): Promise<string[]> {
+    return new Promise<string[]>((resolve, reject) => {
+      fs.readdir(this.filePath(prefix), (err, files) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(files.map((x) => join(prefix, x).replace(/\\/g, "/")));
+        }
+      });
+    });
+  }
+
+  public async set(request: SetAssetRequest): Promise<AssetMetadata> {
+    await new Promise((resolve, reject) => {
+      fs.ensureDir(dirname(this.filePath(request.path)), (err) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve();
+        }
+      });
+    });
+
+    return new Promise<AssetMetadata>((resolve, reject) => {
+      const metadata: AssetMetadata = {
+        path: request.path,
+      };
+
+      const filePath = this.filePath(request.path);
+      if (Buffer.isBuffer(request.source)) {
+        fs.writeFile(filePath, request.source, (writeFileError) => {
+          if (writeFileError) {
+            reject(writeFileError);
+          } else {
+            resolve(metadata);
+          }
+        });
+      } else {
+        request.source.on("error", (err) => reject(err));
+        const writableStream = fs.createWriteStream(filePath);
+        writableStream.on("error", (err) => reject(err));
+
+        request.source.pipe(writableStream);
+        writableStream.on("close", () => {
+          resolve(metadata);
+        });
+      }
+    });
+  }
+
+  private readonly filePath = (path: string) => join(this.options.path, path);
 
 }
--- packages/uno-serverless/test/unit/services/assets-repository-test.ts
+++ packages/uno-serverless/test/unit/services/assets-repository-test.ts
@@ -2,7 +2,7 @@ import { expect } from "chai";
 import { randomBytes } from "crypto";
 import { createReadStream } from "streamifier";
 import { randomStr } from "../../../src/core/utils";
-import { InMemoryAssetsRepository } from "../../../src/services/assets-repository";
+import { FileAssetsRepository, InMemoryAssetsRepository } from "../../../src/services/assets-repository";
 
 describe("InMemoryAssetsRepository", () => {
 
@@ -14,7 +14,7 @@ describe("InMemoryAssetsRepository", () => {
 
   it("should get and set buffers", async () => {
     const source = randomBytes(512);
-    const path = randomStr();
+    const path = "foo/bar";
     const mediaType = randomStr();
 
     const result = await assetsRepo.set({ mediaType, path, source });
@@ -33,7 +33,7 @@ describe("InMemoryAssetsRepository", () => {
   it("should get and set streams", async () => {
     const sourceBuffer = randomBytes(512);
     const source = createReadStream(sourceBuffer);
-    const path = randomStr();
+    const path = "foo/bar";
     const mediaType = randomStr();
 
     const result = await assetsRepo.set({ mediaType, path, source });
@@ -51,7 +51,7 @@ describe("InMemoryAssetsRepository", () => {
 
   it("should clear", async () => {
     const source = randomBytes(512);
-    const path = randomStr();
+    const path = "foo/bar";
     const mediaType = randomStr();
 
     await assetsRepo.set({ mediaType, path, source });
@@ -60,6 +60,127 @@ describe("InMemoryAssetsRepository", () => {
 
     await assetsRepo.clear();
     result = await assetsRepo.get(path);
+    expect(result).to.be.undefined;
+  });
+
+  it("should list", async () => {
+    const path1 = "foo/1";
+    const path2 = "foo/2";
+    const path3 = "bar/1";
+
+    await assetsRepo.set({ mediaType: randomStr(), path: path1, source: randomBytes(512) });
+    await assetsRepo.set({ mediaType: randomStr(), path: path2, source: randomBytes(512) });
+    await assetsRepo.set({ mediaType: randomStr(), path: path3, source: randomBytes(512) });
+
+    let result = await assetsRepo.list("foo");
+    expect(result).to.deep.equal([ path1, path2 ]);
+
+    result = await assetsRepo.list("bar");
+    expect(result).to.deep.equal([ path3 ]);
+  });
+
+  it("should exists", async () => {
+    const path = "foo/bar";
+
+    await assetsRepo.set({ mediaType: randomStr(), path, source: randomBytes(512) });
+
+    let result = await assetsRepo.exists(path);
+    expect(result).to.not.be.undefined;
+
+    result = await assetsRepo.exists(randomStr());
+    expect(result).to.be.undefined;
+  });
+
+});
+
+describe("FileAssetsRepository", () => {
+
+  let assetsRepo: FileAssetsRepository;
+
+  beforeEach(() => {
+    assetsRepo = new FileAssetsRepository();
+  });
+
+  afterEach(async () => {
+    await assetsRepo.clear();
+  });
+
+  it("should get and set buffers", async () => {
+    const source = randomBytes(512);
+    const path = "foo/bar";
+    const mediaType = randomStr();
+
+    const result = await assetsRepo.set({ mediaType, path, source });
+    expect(result.path).to.equal(path);
+    expect(result.mediaType).to.be.undefined;
+    expect(result.etag).to.be.undefined;
+
+    const asset = await assetsRepo.get(path);
+    expect(asset).to.not.be.undefined;
+    expect(asset!.data.compare(source)).to.equal(0);
+    expect(asset!.path).to.equal(result.path);
+    expect(asset!.mediaType).to.equal(result.mediaType);
+    expect(asset!.etag).to.equal(result.etag);
+  });
+
+  it("should get and set streams", async () => {
+    const sourceBuffer = randomBytes(512);
+    const source = createReadStream(sourceBuffer);
+    const path = "foo/bar";
+    const mediaType = randomStr();
+
+    const result = await assetsRepo.set({ mediaType, path, source });
+    expect(result.path).to.equal(path);
+    expect(result.mediaType).to.be.undefined;
+    expect(result.etag).to.be.undefined;
+
+    const asset = await assetsRepo.get(path);
+    expect(asset).to.not.be.undefined;
+    expect(asset!.data.compare(sourceBuffer)).to.equal(0);
+    expect(asset!.path).to.equal(result.path);
+    expect(asset!.mediaType).to.equal(result.mediaType);
+    expect(asset!.etag).to.equal(result.etag);
+  });
+
+  it("should clear", async () => {
+    const source = randomBytes(512);
+    const path = "foo/bar";
+    const mediaType = randomStr();
+
+    await assetsRepo.set({ mediaType, path, source });
+    let result = await assetsRepo.get(path);
+    expect(result).to.not.be.undefined;
+
+    await assetsRepo.clear();
+    result = await assetsRepo.get(path);
+    expect(result).to.be.undefined;
+  });
+
+  it("should list", async () => {
+    const path1 = "foo/1";
+    const path2 = "foo/2";
+    const path3 = "bar/1";
+
+    await assetsRepo.set({ mediaType: randomStr(), path: path1, source: randomBytes(512) });
+    await assetsRepo.set({ mediaType: randomStr(), path: path2, source: randomBytes(512) });
+    await assetsRepo.set({ mediaType: randomStr(), path: path3, source: randomBytes(512) });
+
+    let result = await assetsRepo.list("foo");
+    expect(result).to.deep.equal([ path1, path2 ]);
+
+    result = await assetsRepo.list("bar");
+    expect(result).to.deep.equal([ path3 ]);
+  });
+
+  it("should exists", async () => {
+    const path = "foo/bar";
+
+    await assetsRepo.set({ mediaType: randomStr(), path, source: randomBytes(512) });
+
+    let result = await assetsRepo.exists(path);
+    expect(result).to.not.be.undefined;
+
+    result = await assetsRepo.exists(randomStr());
     expect(result).to.be.undefined;
   });
 
